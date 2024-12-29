079c90ef6da0f94b062f63a57d987082d6de17e5
Amaan <71920621+amaank404@users.noreply.github.com>
2024-12-29 20:32:20 +0530

    Made some neovide specific changes

--- lua/amaank404/init.lua
+++ lua/amaank404/init.lua
@@ -22,3 +22,34 @@ vim.opt.incsearch        = true
 vim.opt.scrolloff        = 8
 
 vim.opt.colorcolumn      = "100"
+
+if vim.g.neovide then
+    vim.g.neovide_scale_factor = 0.8
+    vim.g.neovide_title_background_color = string.format(
+        "%x",
+        vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg
+    )
+
+
+    local function set_ime(args)
+        if args.event:match("Enter$") then
+            vim.g.neovide_input_ime = true
+        else
+            vim.g.neovide_input_ime = false
+        end
+    end
+
+    local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+        group = ime_input,
+        pattern = "*",
+        callback = set_ime
+    })
+
+    vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+        group = ime_input,
+        pattern = "[/\\?]",
+        callback = set_ime
+    })
+end
