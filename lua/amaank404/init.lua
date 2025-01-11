0834ee2913c6fdac05701074325cf01e6020a252
Amaan <71920621+amaank404@users.noreply.github.com>
2025-01-11 15:15:43 +0530

    Added some quick directory changing maps

--- lua/amaank404/init.lua
+++ lua/amaank404/init.lua
@@ -26,7 +26,7 @@ vim.opt.cmdheight        = 2
 
 -- Neovide specific details
 if vim.g.neovide then
-    vim.g.neovide_scale_factor = 0.8
+    vim.g.neovide_scale_factor = 0.7
     vim.g.neovide_title_background_color = string.format(
         "%x",
         vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg
--- lua/amaank404/remap.lua
+++ lua/amaank404/remap.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = " "
 
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fu", "<cmd>tcd ~<CR>")
+vim.keymap.set("n", "<leader>fp", "<cmd>tcd ~/Desktop<CR>")
